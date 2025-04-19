9d2daa64f98547b08884d311f048b433511d3768
Melan24 <melandrimelan86@gmail.com>
2025-04-19 11:15:41 +0700

    ini komit kami pada hari sabtu pada tanggal 19 april 2025

---
+++ class.php
@@ -0,0 +1,34 @@
+<?php
+
+    class kendaraan {
+        public $warna;
+        public $merk;
+        public $tahun;
+        public $harga;
+        public $kecepatan;
+    
+
+    function __construct($warna, $merk, $tahun, $harga, $kecepatan) {
+        $this->warna = $warna;
+        $this->merk = $merk;
+        $this->tahun = $tahun;
+        $this->harga = $harga;
+        $this->kecepatan = $kecepatan;
+    
+    }
+
+    function info () {
+        return "Mobil ini berwarna {$this->warna}, merk {$this->merk}, tahun {$this->tahun}, dan harga {$this->harga}.";
+    }
+
+    function tambahkecepatan($kecepatan) {
+        $this->kecepatan += $kecepatan;
+        return "Kecepatan mobil sekarang adalah {$this->kecepatan} km/jam.";
+    }
+}
+
+$mobil1 = new Kendaraan("Merah", "Toyota", 2020, 300000000, 0);
+echo $mobil1->info();
+echo $mobil1->tambahKecepatan(400);
+
+?>
